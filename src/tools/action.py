2fba8397911af87facf5db1bc272b8ad3d5640ac
mardio <mardio@noreply.gitcode.com>
2026-01-07 12:18:02 +0800

    Action

--- src/tools/action.py
+++ src/tools/action.py
@@ -1,2 +1,15 @@
-from src.tools.prompt import ToolPromptManager
-from src.chat.session import ChatSession
+from src.tools.prompt import ToolPromptManager,prompt_man
+from src.chat.session import ChatSession,chatSession_man
+from src.tools.parser import Parser,parser_man
+import datetime
+from typing import List, Dict, Any, Optional, Iterator
+from dataclasses import dataclass
+
+class Action():
+    def __init__(self):
+        #几个大指针
+        self.chatSession=chatSession_man
+        self.parser=parser_man
+        self.prompt=prompt_man
+
+action_man=Action()
